53b50c8df2273162d8966bfbfc0ff86cba1033b9
Juma Stevens <jumastevens@gmail.com>
2020-03-20 11:39:50 -0400

    make env vars strings

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/auth'
 import 'firebase/functions'
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`
 }
 
 if (!firebase.apps.length) firebase.initializeApp(config)
