f560870fd3344c3d39fbcb3a60d118e8c726ca2a
Juma Stevens <jumastevens@gmail.com>
2020-03-20 11:52:05 -0400

    hardcode firebase vars

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/auth'
 import 'firebase/functions'
 
 const config = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-  databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`
+  apiKey: 'AIzaSyCmUhXP9198N0y8vjuc0_gEO6XVdGCldws',
+  authDomain: 'share-overflow.firebaseapp.com',
+  databaseURL: 'https://share-overflow.firebaseio.com',
+  projectId: 'share-overflow',
+  storageBucket: 'share-overflow.appspot.com',
+  messagingSenderId: '150551217325',
+  appId: '1:150551217325:web:8401cdcf3f378eaa027224'
 }
 
 if (!firebase.apps.length) firebase.initializeApp(config)
