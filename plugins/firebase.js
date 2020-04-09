12b1c24a22a0992e0f03eeaa91b3bf2d8ecaf4ec
Juma Stevens <jumastevens@gmail.com>
2020-04-09 18:02:07 -0400

    update

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,15 +4,15 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/functions'
 
-// const prodConfig = {
-//   apiKey: 'AIzaSyAa4jRHDqgEpkyWhbRR1L15BfzWuU6hYeo',
-//   authDomain: 'girls-travel-co.firebaseapp.com',
-//   databaseURL: 'https://girls-travel-co.firebaseio.com',
-//   projectId: 'girls-travel-co',
-//   storageBucket: 'girls-travel-co.appspot.com',
-//   messagingSenderId: '1001380734364',
-//   appId: '1:1001380734364:web:994e56a46216c58ff1d459'
-// }
+const prodConfig = {
+  apiKey: 'AIzaSyAa4jRHDqgEpkyWhbRR1L15BfzWuU6hYeo',
+  authDomain: 'girls-travel-co.firebaseapp.com',
+  databaseURL: 'https://girls-travel-co.firebaseio.com',
+  projectId: 'girls-travel-co',
+  storageBucket: 'girls-travel-co.appspot.com',
+  messagingSenderId: '1001380734364',
+  appId: '1:1001380734364:web:994e56a46216c58ff1d459'
+}
 
 const devConfig = {
   apiKey: 'AIzaSyDZ0EkjpJeEN8Ho7PK-DCd4qZgxfDwN_JY',
@@ -24,13 +24,13 @@ const devConfig = {
   appId: '1:759661404696:web:71e834895529129be7996d'
 }
 
-// const isProd = process.env.NODE_ENV === 'production'
-// console.log('isProd: ', isProd)
+const isProd = process.env.NODE_ENV === 'production'
+console.log('isProd: ', isProd)
 
-// if (!firebase.apps.length)
-//   firebase.initializeApp(isProd ? prodConfig : devConfig)
+if (!firebase.apps.length)
+  firebase.initializeApp(isProd ? prodConfig : devConfig)
 
-if (!firebase.apps.length) firebase.initializeApp(devConfig)
+// if (!firebase.apps.length) firebase.initializeApp(devConfig)
 
 // firebase.functions().useFunctionsEmulator('http://localhost:5000')
 
